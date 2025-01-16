c076b183ce0aaf3909c1589f9bc74ad2be5545a7
RubinoD1 <Rubinod1@gmail.com>
2025-01-16 12:31:05 -0500

    updated array

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,13 @@
 // DOM 
+const square1 = document.getElementById("1");
+const square2 = document.getElementById("2");
+const square3 = document.getElementById("3");
+const square4 = document.getElementById("4");
+const square5 = document.getElementById("5");
+const square6 = document.getElementById("6");
+const square7 = document.getElementById("7");
+const square8 = document.getElementById("8");
+const square9 = document.getElementById("9");
 
 // used to set class value for icon image
 const circle = "o";
@@ -18,7 +27,7 @@ document
       // change turn value 
       changeTurn();
     } else {
-      console.log("filled");
+      console.log("filled");//REPLACE WITH break instead once testing done
     }
 
   });
@@ -38,3 +47,41 @@ function changeTurn(){
 }
 
 // Check if game won || is draw ELSE continue
+function gameCheck(value){
+  // set value to var  square1.innerHTML
+  let check = value;
+  if(check.includes("x") == true){//chain an & .includes("o") also true for invalid check 
+    console.log("true");
+    
+  }else {
+    console.log("false");
+    
+  }
+}
+
+
+let array = [
+  //horizontal row 1
+  {row: [square1.innerHTML,square2.innerHTML,square3.innerHTML], winnable: true},
+  //horizontal row 2 
+  {row: [square4.innerHTML + square5.innerHTML + square6.innerHTML], winnable: true},
+  //horizontal row 3
+  {row: [7,8,9], winnable: true},
+  //vertical row 1
+  {row: [1,4,7], winnable: true},
+  //vertical row 2
+  {row: [2,5,8], winnable: true},
+  //vertical row 3
+  {row: [3,6,9], winnable: true},
+  //diagonal row 1 
+  {row: [7,5,3], winnable: true},
+  //diagonal row 2 
+  {row: [1,5,9], winnable: true},
+ ];
+ 
+ // console.log(array[0].row);
+ // console.log(array[1].winnable);
+ console.log(array[0].row, array[1].row);
+
+//  gameCheck(array[0].row);
+//  gameCheck(array[1].row);
